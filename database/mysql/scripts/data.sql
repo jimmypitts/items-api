c07c644e3208de49c9d5d2ae91eec4be9becb7c7
Jimmy Pitts <jimmyrp@gmail.com>
2016-06-02 18:14:13 -0700

    [UPDATE] Added test data scripts

---
+++ database/mysql/scripts/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO `item` (title,description,category,price,name,latitude,longitude,status,create_date)
+VALUES
+	('Arm Chair', 'Very nice armchair', 'Furniture', 20.12, 'Bob Henry', 12.2351, 19.2123, 'Available', 1464895886),
+	('Leg Chair', 'Very nice legchair', 'Furniture', 456.12, 'Joe Henry', 12.2351, 42.2934, 'Available', 1464895886),
+	('Sweet Bike', 'Get from A to B in style', 'Bikes', 250, 'Jack Ryan', 34.51, 84.28475, 'Banned', 1464895886)
+
+;
