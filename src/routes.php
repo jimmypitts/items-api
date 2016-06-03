56e13e80ded6ab61466a7e10eb092f95a0bbc066
Jimmy Pitts <jimmyrp@gmail.com>
2016-06-02 18:16:42 -0700

    [UPDATE] Modify routes/controller

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,27 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$this->get('/api/item/[{id}]', function ($request, $response, $args) {
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    $item = Model_Item::get_by_id($args['id']);
+    if (!$item)
+    {
+        return $response->write(json_encode(array('status' => 'Error')))->withStatus(404);
+    }
+
+    return $response->write(json_encode($item->to_array()));
+});
+
+$this->get('/api/items', function ($request, $response, $args) {
+
+    $seller = $request->getQueryParam('seller');
+    $category = $request->getQueryParam('category');
+
+    $res = array();
+    $items = Model_Item::get_items_by_seller_and_category($seller, $category);
+    foreach ($items as $item)
+    {
+        $res[] = $item->to_array();
+    }
+    $response->write(json_encode(array('items' => $res)));
 });
